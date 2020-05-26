ba0ad32ee5cbfe8b5f5ea2c862cd7702a3a6eea2
LukasTsunami <prateadow@gmail.com>
2020-05-26 13:54:16 -0300

    creating clear method on stack

--- src/lib/Stack.js
+++ src/lib/Stack.js
@@ -2,6 +2,8 @@ class Stack {
   constructor() {
     this.items = [];
   }
+
+  clear = () => (this.items = []);
   count = () => this.items.length;
   isEmpty = () => this.items.length === 0;
   peek = () => this.items[this.items.length - 1];
