6a34a2b031740353012ada6fcf2e2739c9aa687c
LukasTsunami <prateadow@gmail.com>
2020-05-26 13:01:38 -0300

    Testing for peeking the last item on stack and refactoring add method to use currently test peek method

--- src/lib/Stack.js
+++ src/lib/Stack.js
@@ -2,7 +2,6 @@ class Stack {
   constructor() {
     this.items = [];
   }
-
   peek = () => this.items[this.items.length - 1];
   push = (item) => this.items.push(item);
 }
--- src/lib/__tests__/Stack.test.js
+++ src/lib/__tests__/Stack.test.js
@@ -1,11 +1,25 @@
 const Stack = require("../Stack");
 
+describe("requesting for the stack's head", () => {
+  beforeEach(() => {
+    stack = new Stack();
+
+    Object.defineProperty(stack, "items", {
+      get: jest.fn(() => [1, 2, 8]),
+    });
+  });
+
+  it("should get it", () => {
+    expect(stack.peek()).toBe(8);
+  });
+});
+
 describe("adding a new item to stack", () => {
   beforeEach(() => {
     stack = new Stack();
   });
 
-  it("should be added on top of stack", () => {
+  it("should be added on stack's head", () => {
     const new_element = 9;
     stack.push(new_element);
     expect(stack.peek()).toBe(9);
