39c5816e7d2ccb2ab6e54a4b001836fded1e67c2
LukasTsunami <prateadow@gmail.com>
2020-05-26 15:20:30 -0300

    Creating test to check private members encapsulation

--- src/lib/__tests__/Stack.test.js
+++ src/lib/__tests__/Stack.test.js
@@ -1,140 +1,151 @@
 const Stack = require("../Stack");
 
-describe("requesting for see stack's head", () => {
-  beforeEach(() => {
-    stack = new Stack();
+describe("#stack_encapsulation", () => {
+  describe("trying to access private members", () => {
+    it("should not be able to access them", () => {
+      stack = new Stack();
+      expect(stack.items).toThrow();
+    });
+  });
+});
 
-    Object.defineProperty(stack, "items", {
-      get: jest.fn(() => [1, 2, 8]),
+describe("#stack_methods", () => {
+  describe("requesting for see stack's head", () => {
+    beforeEach(() => {
+      stack = new Stack();
+
+      Object.defineProperty(stack, "items", {
+        get: jest.fn(() => [1, 2, 8]),
+      });
+    });
+
+    it("should see it", () => {
+      expect(stack.peek()).toBe(8);
     });
   });
 
-  it("should see it", () => {
-    expect(stack.peek()).toBe(8);
-  });
-});
+  describe("counting how much items are on stack", () => {
+    beforeEach(() => {
+      stack = new Stack();
 
-describe("counting how much items are on stack", () => {
-  beforeEach(() => {
-    stack = new Stack();
+      Object.defineProperty(stack, "items", {
+        get: jest.fn(() => [2, 2, 8]),
+      });
+    });
 
-    Object.defineProperty(stack, "items", {
-      get: jest.fn(() => [2, 2, 8]),
+    it("should get it", () => {
+      expect(stack.count()).toBe(3);
     });
   });
 
-  it("should get it", () => {
-    expect(stack.count()).toBe(3);
+  describe("pushing a new item to stack", () => {
+    it("should be pushed on stack's head", () => {
+      stack = new Stack();
+      const new_element = 9;
+      const old_count = stack.count();
+      stack.push(new_element);
+      expect(stack.peek()).toBe(9);
+      expect(stack.count()).toBe(old_count + 1);
+    });
   });
-});
 
-describe("pushing a new item to stack", () => {
-  it("should be pushed on stack's head", () => {
+  describe("popping an item from stack", () => {
+    it("should be returned", () => {
+      stack = new Stack();
+      stack.push(9);
+      stack.push(2);
+      stack.push(4);
+
+      expect(stack.pop()).toBe(4);
+      expect(stack.pop()).toBe(2);
+      expect(stack.pop()).toBe(9);
+      expect(stack.pop()).toBe(undefined);
+    });
+  });
+
+  test("if stack is empty", () => {
     stack = new Stack();
-    const new_element = 9;
-    const old_count = stack.count();
-    stack.push(new_element);
-    expect(stack.peek()).toBe(9);
-    expect(stack.count()).toBe(old_count + 1);
-  });
-});
 
-describe("popping an item from stack", () => {
-  it("should be returned", () => {
-    stack = new Stack();
-    stack.push(9);
-    stack.push(2);
-    stack.push(4);
-
-    expect(stack.pop()).toBe(4);
-    expect(stack.pop()).toBe(2);
-    expect(stack.pop()).toBe(9);
-    expect(stack.pop()).toBe(undefined);
-  });
-});
-
-test("if stack is empty", () => {
-  stack = new Stack();
-
-  expect(stack.isEmpty()).toBeTruthy();
-
-  stack.push(9);
-
-  expect(stack.isEmpty()).toBeFalsy();
-});
-
-describe("clearing the stack", () => {
-  beforeEach(() => {
-    stack = new Stack();
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
-    stack.push(4);
-  });
-
-  it("should be empty", () => {
-    expect(stack.count()).toBe(4);
-    stack.clear();
     expect(stack.isEmpty()).toBeTruthy();
-    expect(stack.count()).toBe(0);
-  });
-});
 
-describe("printing the stack", () => {
-  beforeEach(() => {
-    stack = new Stack();
+    stack.push(9);
+
+    expect(stack.isEmpty()).toBeFalsy();
   });
 
-  describe("with numbers", () => {
-    it("should return a string with all items from last to first", () => {
+  describe("clearing the stack", () => {
+    beforeEach(() => {
+      stack = new Stack();
       stack.push(1);
       stack.push(2);
       stack.push(3);
       stack.push(4);
-      expect(stack.toString()).toBe("4,3,2,1");
+    });
+
+    it("should be empty", () => {
+      expect(stack.count()).toBe(4);
+      stack.clear();
+      expect(stack.isEmpty()).toBeTruthy();
+      expect(stack.count()).toBe(0);
     });
   });
 
-  describe("with strings", () => {
-    it("should return a string with all items from last to first", () => {
-      stack.push(1);
-      stack.push("b");
-      stack.push(3);
-      stack.push("d");
-      expect(stack.toString()).toBe("d,3,b,1");
+  describe("printing the stack", () => {
+    beforeEach(() => {
+      stack = new Stack();
     });
-  });
 
-  describe("with objects", () => {
-    it("should print their assignature inside returned string", () => {
-      const person = {
-        name: "Lucas",
-        age: 23,
-      };
-
-      stack.push(1);
-      stack.push(person);
-      stack.push("4");
-      expect(stack.toString()).toBe('4,{"name":"Lucas","age":23},1');
+    describe("with numbers", () => {
+      it("should return a string with all items from last to first", () => {
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        expect(stack.toString()).toBe("4,3,2,1");
+      });
     });
-  });
 
-  describe("with class objects", () => {
-    class SomeClass {
-      constructor() {}
+    describe("with strings", () => {
+      it("should return a string with all items from last to first", () => {
+        stack.push(1);
+        stack.push("b");
+        stack.push(3);
+        stack.push("d");
+        expect(stack.toString()).toBe("d,3,b,1");
+      });
+    });
 
-      toString() {
-        return "I'm some class return string";
+    describe("with objects", () => {
+      it("should print their assignature inside returned string", () => {
+        const person = {
+          name: "Lucas",
+          age: 23,
+        };
+
+        stack.push(1);
+        stack.push(person);
+        stack.push("4");
+        expect(stack.toString()).toBe('4,{"name":"Lucas","age":23},1');
+      });
+    });
+
+    describe("with class objects", () => {
+      class SomeClass {
+        constructor() {}
+
+        toString() {
+          return "I'm some class return string";
+        }
       }
-    }
 
-    it("should print their assignature inside returned string", () => {
-      const someClassObject = new SomeClass();
+      it("should print their assignature inside returned string", () => {
+        const someClassObject = new SomeClass();
 
-      stack.push(1);
-      stack.push(someClassObject);
-      stack.push("4");
-      expect(stack.toString()).toBe("4,I'm some class return string,1");
+        stack.push(1);
+        stack.push(someClassObject);
+        stack.push("4");
+        expect(stack.toString()).toBe("4,I'm some class return string,1");
+      });
     });
   });
 });
